c4b0b78f20221b6b3856ec5a40b62d960109437b
Jason-Guo1999 <106792458+Jason-Guo1999@users.noreply.github.com>
2023-11-12 02:52:03 +0000

    final commit

---
+++ lib/extract.py
@@ -0,0 +1,11 @@
+"""
+Extract a dataset from a URL
+"""
+import requests
+
+def extract(url,file_path):
+    """Extract a url to a file path"""
+    with requests.get(url) as r:
+        with open(file_path, 'wb') as f:
+            f.write(r.content)
+    return file_path
